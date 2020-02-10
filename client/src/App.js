0c7dd1eb174ce0775fddf36dcd2916f0aecb5c62
Justin Singh <justin.singh125125@gmail.com>
2020-02-10 11:13:23 -0800

    coverted to react hooks

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,18 @@
-import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
-import Navbar from "./components/Navbar";
+import Navbar from './components/Navbar';
 
-import Index from "./pages/index";
-import Saved from "./pages/saved";
+import Index from './pages/index';
+import Saved from './pages/saved';
 
-class App extends React.Component {
-
-  state = {
-    currentPage: window.location.pathname
-  }
-
-
-  handlePageChange = (page) => {
-    this.setState({ currentPage: page })
-  }
-
-  render() {
-
-    return (
-      <Router>
-        <div>
-          <Navbar currentPage={this.state.currentPage} handlePageChange={this.handlePageChange} />
-          <Route exact path="/" component={Index} />
-          <Route exact path="/saved" component={Saved} />
-        </div>
-      </Router>
-    );
-  }
-}
-
+const App = () => {
+	return (
+		<Router>
+			<Navbar />
+			<Route exact path='/' component={Index} />
+			<Route exact path='/saved' component={Saved} />
+		</Router>
+	);
+};
 export default App;
