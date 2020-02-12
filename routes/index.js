4393924872b945c9bf87bc2b8e7cf603c9130aba
Justin Singh <justin.singh125125@gmail.com>
2020-02-12 11:24:45 -0800

    ready for deployment

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,6 @@ const apiRoutes = require('./api');
 app.use('/api', apiRoutes);
 
 // If no API routes are hit, send the React app
-app.use((req, res) => res.sendFile(path.join(__dirname, '../../client/build/index.html')));
+app.use((req, res) => res.sendFile(path.join(__dirname, '../client/build/index.html')));
 
 module.exports = app;
